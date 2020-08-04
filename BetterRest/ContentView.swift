ef10bd6058800ca12ec291f4c66574945b200bf6
tle2693 <toanle2612@outlook.com>
2020-08-04 10:46:05 -0400

    Initial Commit

---
+++ BetterRest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by Toan Le on 8/4/20.
+//  Copyright © 2020 TL. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
